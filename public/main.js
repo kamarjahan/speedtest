11bcb381ffe511d5f4e07e7dd9d2f97c6b3750ad
KAMARJAHAN <ztenkammu@gmail.com>
2024-07-07 12:02:10 +0530

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,9 +1,18 @@
-
 document.getElementById('start-test').addEventListener('click', async () => {
-    const response = await fetch('/api/speedtest');
-    const data = await response.json();
-    
-    document.getElementById('ping').textContent = data.ping.latency;
-    document.getElementById('download').textContent = (data.download.bandwidth / 125000).toFixed(2);
-    document.getElementById('upload').textContent = (data.upload.bandwidth / 125000).toFixed(2);
+    document.getElementById('loading').style.display = 'block';
+    document.getElementById('results').style.display = 'none';
+
+    try {
+        const response = await fetch('/api/speedtest');
+        const data = await response.json();
+        
+        document.getElementById('ping').textContent = data.ping.latency;
+        document.getElementById('download').textContent = (data.download.bandwidth / 125000).toFixed(2);
+        document.getElementById('upload').textContent = (data.upload.bandwidth / 125000).toFixed(2);
+    } catch (error) {
+        console.error('Error fetching speed test data:', error);
+    }
+
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('results').style.display = 'flex';
 });
