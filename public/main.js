e3e3112fef226800dc41134de1782d559e489dc1
KAMARJAHAN <ztenkammu@gmail.com>
2024-07-07 11:43:43 +0530

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,1 +1,9 @@
 
+document.getElementById('start-test').addEventListener('click', async () => {
+    const response = await fetch('/api/speedtest');
+    const data = await response.json();
+    
+    document.getElementById('ping').textContent = data.ping.latency;
+    document.getElementById('download').textContent = (data.download.bandwidth / 125000).toFixed(2);
+    document.getElementById('upload').textContent = (data.upload.bandwidth / 125000).toFixed(2);
+});
